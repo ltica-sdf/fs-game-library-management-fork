5bcb49bad5b1cae52710a71ca7aaa23e9b9e0d30
Mikhail <mgerachshenko@academic.rrc.ca>
2026-01-15 20:36:33 -1000

    Added featured game scrollable list, fixed main importing from a non-existent file, and added game card images to public

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-
+import "./App.css";
+import Nav from "./components/nav/Nav.tsx";
+import FeaturedGames from "./components/FeaturedGames/FeaturedGames";
 
 function App() {
   return (
     <>
-    
+      <Nav />
+      <main>
+        <FeaturedGames />
+      </main>
     </>
   );
-};
+}
 
-export default App
+export default App;
---
+++ src/components/FeaturedGames/FeaturedGames.tsx
@@ -0,0 +1,33 @@
+import "./FeaturedGames.css";
+
+type FeaturedGame = {
+  id: number;
+  title: string;
+  image: string;
+};
+
+export default function FeaturedGames() {
+  const featuredGames: FeaturedGame[] = [
+    { id: 1, title: "EarthBound", image: "/EB.jpg" },
+    { id: 2, title: "The Legend of Zelda", image: "/LOZOOT.jpg" },
+    { id: 3, title: "Pokemon Platinum", image: "/PP.png" },
+    { id: 4, title: "Super Mario 64", image: "/SM64.jpg" },
+    { id: 5, title: "Goldeneye", image: "/Goldeneye.jpg" },
+    { id: 5, title: "Super Mario World", image: "/SMW.png" },
+  ];
+
+  return (
+    <section className="featured-games">
+      <h2>Featured Games</h2>
+
+      <ul className="featured-games-list">
+        {featuredGames.map((game) => (
+          <li key={game.id} className="featured-game-card">
+            <img src={game.image} alt={game.title} />
+            <span className="game-title">{game.title}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
